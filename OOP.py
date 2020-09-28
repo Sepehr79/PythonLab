344f9c572bc55605c9f96378ac35ebfc63b2a361
Sepehr79 <68536625+Sepehr79@users.noreply.github.com>
2020-09-28 17:18:45 +0330

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 #OOP in python
 
+
+#defining enum for gender
 class Gender(Enum):
     MALE = 1
     FAMALE = 2
@@ -12,9 +14,36 @@ class Person:
         self.lastName = lastName
         self.age = age
         self.gender = gender
+    
+    #__str__ method return string of object
+    
     def __str__(self):
         return "name: {} lastName: {} age: {} gender: {}".format(self.name, self.lastName, self.age, self.gender)
+        
+    #methods to comparison objects    
+        
+    def __eq__(self, other):
+        if(isinstance(self, other)):
+            return (self.age == other.age)
+    def __ne__(self, other):
+        return (self.age != other.age)
 
-person = Person("sepehr", "mollaei", 20, Gender.MALE.name)
-print(person)
+    def __lt__(self, other):
+        return (self.age < other.age)
+
+    def __le__(self, other):
+        return (self.age <= other.age)
+
+    def __gt__(self, other):
+        return (self.age > other.age)
+
+    def __ge__(self, other):
+        return (self.age >= other.age)
+        
+        
+sepehr = Person("sepehr", "mollaei", 20, Gender.MALE.name)
+ali = Person("ali", "mohammadi", 21, Gender.MALE.name)
+
+if ali > sepehr:
+    print("ali is grater!")
 
